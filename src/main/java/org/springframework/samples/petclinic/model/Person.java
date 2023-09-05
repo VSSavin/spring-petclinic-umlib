cf23641777028eb028a5804c1eadde91f135cb8f
vssavin <savin1801@gmail.com>
2023-09-05 11:38:30 +0300

    Person: first_name and last_name - @NotBlank validation returned

--- src/main/java/org/springframework/samples/petclinic/model/Person.java
+++ src/main/java/org/springframework/samples/petclinic/model/Person.java
@@ -17,6 +17,7 @@ package org.springframework.samples.petclinic.model;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotBlank;
 
 /**
  * Simple JavaBean domain object representing an person.
@@ -27,9 +28,11 @@ import javax.persistence.MappedSuperclass;
 public class Person extends BaseEntity {
 
 	@Column(name = "first_name")
+	@NotBlank
 	private String firstName;
 
 	@Column(name = "last_name")
+	@NotBlank
 	private String lastName;
 
 	public String getFirstName() {
