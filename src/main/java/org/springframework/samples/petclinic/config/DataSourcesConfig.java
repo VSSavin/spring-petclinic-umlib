aaf2d672519590ad7b4a02ee793cca10233611c6
vssavin <savin1801@gmail.com>
2023-12-26 12:43:35 +0300

    added petclinic configuration classes

---
+++ src/main/java/org/springframework/samples/petclinic/config/DataSourcesConfig.java
@@ -0,0 +1,37 @@
+package org.springframework.samples.petclinic.config;
+
+import com.github.vssavin.usmancore.config.UsmanDataSourceConfig;
+import org.springframework.context.annotation.*;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+
+import javax.sql.DataSource;
+
+import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.H2;
+
+/**
+ * @author vssavin on 26.12.2023.
+ */
+@Configuration
+@Import(UsmanDataSourceConfig.class)
+public class DataSourcesConfig {
+
+	private DataSource appDataSource;
+
+	@Bean
+	@Primary
+	public DataSource appDatasource() {
+		if (appDataSource == null) {
+			appDataSource = new EmbeddedDatabaseBuilder(
+					new DefaultResourceLoader(UsmanDataSourceConfig.class.getClassLoader()))
+				.generateUniqueName(true)
+				.setType(H2)
+				.setScriptEncoding("UTF-8")
+				.ignoreFailedDrops(true)
+				.addScript("com/github/vssavin/usmancore/config/init.sql")
+				.build();
+		}
+		return appDataSource;
+	}
+
+}
